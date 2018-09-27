bce3737022252104158dbcd275436b5ea7672da6
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-09-27 17:50:30 -0300

    iconos nav bar

--- src/Components/Navs/IconNav.js
+++ src/Components/Navs/IconNav.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {  NavLink, } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
 
 const IconNav = () => {
   return (
    <div className="card white">
-       <NavLink to='/home'> <i className="far fa-calendar-alt blue-text text-darken-2"></i>Inicio</NavLink>
-       <NavLink to='/friends'> <i className="far fa-calendar-alt blue-text text-darken-2"></i>amigos</NavLink>
+       <NavLink to='/home'> <i className="fas fa-home blue-text text-darken-2"></i>Inicio</NavLink>
+       <NavLink to='/friends'> <i className="fas fa-user-friends blue-text text-darken-2"></i>amigos</NavLink>
        <NavLink to='/events'> <i className="far fa-calendar-alt blue-text text-darken-2"></i>eventos</NavLink>
-       <NavLink to='/games'> <i className="far fa-calendar-alt blue-text text-darken-2"></i>juegos</NavLink>
+       <NavLink to='/games'> <i className="fas fa-trophy blue-text text-darken-2"></i>juegos</NavLink>
     </div>
   )
 }
-export default (IconNav)
+export default (IconNav) 
