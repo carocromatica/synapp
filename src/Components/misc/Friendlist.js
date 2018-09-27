c84bc082f06a4c0a2c77251a27e8f52a494969ff
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-09-27 19:56:02 -0300

    lista de amigos

--- src/Components/misc/Friendlist.js
+++ src/Components/misc/Friendlist.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 
-const amigos = [{nombre: 'Alicia Bárcena', foto: "https://image.flaticon.com/icons/svg/53/53129.svg"
-}, {nombre: 'Héctor Abad', foto: 'https://image.flaticon.com/icons/svg/53/53060.svg'
-}, {nombre: 'Alberto Acosta', foto: 'https://image.flaticon.com/icons/svg/53/53104.svg'
-}, {nombre: 'Lourdes Casanova', foto: 'https://image.flaticon.com/icons/svg/53/53101.svg'
-}, {nombre: 'Manuel Alcántara', foto: 'https://image.flaticon.com/icons/svg/53/53106.svg'
-}, {nombre: 'Christina Figueres', foto: 'https://image.flaticon.com/icons/svg/53/53176.svg'
-}, {nombre: 'Jorge Mario', foto: 'https://image.flaticon.com/icons/svg/53/53157.svg'
+const amigos = [{nombre: 'Alicia Bárcena', foto: "https://randomuser.me/api/portraits/women/7.jpg"
+}, {nombre: 'Héctor Abad', foto: 'https://randomuser.me/api/portraits/men/19.jpg'
+}, {nombre: 'Alberto Acosta', foto: 'https://randomuser.me/api/portraits/men/41.jpg'
+}, {nombre: 'Lourdes Casanova', foto: 'https://randomuser.me/api/portraits/women/41.jpg'
+}, {nombre: 'Manuel Alcántara', foto: 'https://randomuser.me/api/portraits/men/60.jpg'
+}, {nombre: 'Christina Figueres', foto: 'https://randomuser.me/api/portraits/women/34.jpg'
+}, {nombre: 'Jorge Mario', foto: 'https://randomuser.me/api/portraits/men/20.jpg'
 }];
 
 class Friendlist extends Component {
     render(){
         return(
-            <div className="containerFriends">
+            <div>
                 {amigos.map((e, i) => {
                     let num = i + 1; // esto genera el id de las fotos de los amigos
                     return (
-                        <div className="card blue-grey darken-1" key={num}>
-                            <p className="friendName"> {`${e.nombre}`}</p>
-                            <img className="foto" src={e.foto} width="100px" height="100px" alt={e.nombre}/>
+                        <div>
+                        <div className="card white friendCard" key={num}>
+                        <li className="hrz"><img className="avatar" src={e.foto} width="60px" height="60px" alt={e.nombre}
+                        /></li>
+                         <li className="hrz"> {`${e.nombre}`}</li>
+                        </div>
                         </div>
                     )
                 })}
-  
             </div>
         )
     }
