06ebf82a3811057ca7cbedbcb6204d14d74b5182
antonella <antonella_caroca@hotmail.com>
2018-09-28 00:34:32 -0300

    nuevo commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import App2 from '../Components/App2';
-import {  NavLink, } from "react-router-dom";
+//import {  NavLink, } from "react-router-dom";
 import IconNav from '../Components/Navs/IconNav';
+import Post from '../Components/Post/Post';
+import PostForm from '../Components/PostForm/PostForm';
  
 const Home = () => (
   <div className="Container">
@@ -12,16 +14,10 @@ const Home = () => (
 
  <IconNav/>
     <h2>HOLA SOY COMENTARIOS</h2>
-<<<<<<< HEAD
-  
-<App2 />
-
- 
-    
-
-=======
+    <App2 />
+    <Post />
+    <PostForm />
   </div>
->>>>>>> upstream/master
   </div>
 )
 
